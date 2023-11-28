26562dc85b688f004d6859a9d0ef89dc9848fd2c
Peyton Mastropolo <amyhytwn@aol.com>
2023-11-28 00:05:43 -0600

    Update awsConfig.js
    added comments

--- server/config/awsConfig.js
+++ server/config/awsConfig.js
@@ -1,11 +1,15 @@
+// LOAD AWS SDK
 const AWS = require("aws-sdk");
 
+// LOAD .env VARIABLES
 require("dotenv").config();
 
+// SET AWS CONFIG
 AWS.config.update({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_REGION 
-      });
-      
-      module.exports = AWS;
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,         // ACCESS KEY
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // SECRET KEY
+    region: process.env.AWS_REGION                       // REGION
+});
+
+// EXPORT AWS
+module.exports = AWS;
