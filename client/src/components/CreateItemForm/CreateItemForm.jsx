c2460fd1d489d5adb3f8ae0015b2441b5e82d8bd
Peyton Mastropolo <amyhytwn@aol.com>
2023-11-28 10:18:55 -0600

    Update CreateItemForm.jsx
    comments

--- client/src/components/CreateItemForm/CreateItemForm.jsx
+++ client/src/components/CreateItemForm/CreateItemForm.jsx
@@ -16,69 +16,66 @@ import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 // AWS Configuration  
-const bucketName = "our-group-project-last-one-three";
-const accessKeyId = "AKIA3KMU7OYO4F4E7GDM";
-const secretAccessKey = "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";
-const region = 'us-east-1';
+const bucketName = "our-group-project-last-one-three";                   // BUCKET NAME FOR AWS S3
+const accessKeyId = "AKIA3KMU7OYO4F4E7GDM";                              // AWS ACCESS KEY ID
+const secretAccessKey = "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";      // AWS SECRET ACCESS KEY
+const region = 'us-east-1';                                              // AWS REGION
 
 // INITIALIZE S3 CLIENT
 const client = new S3Client({ 
   credentials: { 
-    accessKeyId, 
-    secretAccessKey
+    accessKeyId,         // SET ACCESS KEY ID
+    secretAccessKey      // SET SECRET ACCESS KEY
   },
-  region
+  region                 // SET REGION
 });
 
 // CREATE ITEM FORM COMPONENT
 export default function CreateItemForm({
-  communityName,
-  closeModal,
-  communityId,
+  communityName, // PROPS: COMMUNITY NAME
+  closeModal, // PROPS: FUNCTION TO CLOSE MODAL
+  communityId, // PROPS: COMMUNITY ID
 }) {
   // STATE AND MUTATION FOR CREATING ITEM
   const [createItem, { error, data }] = useMutation(ADD_ITEM, {
     refetchQueries: [
       // REFETCH QUERIES AFTER MUTATION
       {
-        query: QUERY_COMMUNITY_ITEMS,
-        variables: {
-          communityId: communityId,
-        },
+        query: QUERY_COMMUNITY_ITEMS, // QUERY TO REFETCH COMMUNITY ITEMS
+        variables: { communityId: communityId }, // PASS COMMUNITY ID AS VARIABLE
       },
       {
-        query: QUERY_MY_HOARD,
-        variables: {
-          communityId: communityId,
-        },
+        query: QUERY_MY_HOARD, // QUERY TO REFETCH USER'S HOARD
+        variables: { communityId: communityId }, // PASS COMMUNITY ID AS VARIABLE
       },
       // {
-        // query: QUERY_MY_COMMUNITIES
+        // query: QUERY_MY_COMMUNITIES // UNCOMMENT TO REFETCH COMMUNITIES
       // }
     ],
   });
-  const [isPublic, setIsPublic] = useState(false);
+  const [isPublic, setIsPublic] = useState(false); // STATE FOR ITEM VISIBILITY
   const [formState, setFormState] = useState({
-    name: "",
-    description: "",
-    isPublic: isPublic,
+    name: "", // FORM STATE FOR ITEM NAME
+    description: "", // FORM STATE FOR ITEM DESCRIPTION
+    isPublic: isPublic, // FORM STATE FOR ITEM VISIBILITY
   });
+}
 
-  const [file, setFile] = useState();
+const [file, setFile] = useState(); // STATE FOR FILE UPLOAD
 
-  const handleFormChange = (event) => {
-    const { name, value } = event.target;
+const handleFormChange = (event) => { // FUNCTION TO HANDLE FORM CHANGES
+  const { name, value } = event.target; // DESTRUCTURING NAME AND VALUE FROM EVENT TARGET
 
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+  setFormState({ // UPDATE FORM STATE
+    ...formState, // PRESERVE PREVIOUS STATE VALUES
+    [name]: value, // UPDATE SPECIFIC FIELD BASED ON NAME
+  });
+};
 
-  const handleIsPublic = () => {
-    setIsPublic(!isPublic);
-    setFormState({ ...formState, isPublic: !isPublic });
-  };
+const handleIsPublic = () => { // FUNCTION TO TOGGLE PUBLIC STATE
+  setIsPublic(!isPublic); // TOGGLE ISPUBLIC STATE
+  setFormState({ ...formState, isPublic: !isPublic }); // UPDATE FORM STATE WITH NEW ISPUBLIC VALUE
+};
 
   const createNewItem = async () => {
     event.preventDefault();
