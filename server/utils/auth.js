5b1e96ca5a1aa4fc821732178773c87dd8f7f6bd
Peyton Mastropolo <amyhytwn@aol.com>
2023-11-28 00:26:44 -0600

    Update auth.js
    comments

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,26 +1,40 @@
+// IMPORT JSON WEB TOKEN
 const jwt = require("jsonwebtoken");
+
+// IMPORT GRAPHQL ERROR
 const { GraphQLError } = require("graphql");
 
+// DEFINE SECRET KEY
 const secret = "keepinonthedl";
+
+// DEFINE TOKEN EXPIRATION
 const expiration = "2h";
 
+// EXPORT MODULE
 module.exports = {
+  // CUSTOM AUTHENTICATION ERROR
   AuthenticationError: new GraphQLError("Could not authenticate user.", {
     extensions: {
       code: "UNAUTHENTICATED",
     },
   }),
+
+  // AUTH MIDDLEWARE FUNCTION
   authMiddleware: function ({ req }) {
+    // GET TOKEN FROM REQUEST
     let token = req.body.token || req.query.token || req.headers.authorization;
 
+    // EXTRACT TOKEN FROM AUTHORIZATION HEADER
     if (req.headers.authorization) {
       token = token.split(" ").pop().trim();
     }
 
+    // RETURN REQUEST IF NO TOKEN
     if (!token) {
       return req;
     }
 
+    // VERIFY TOKEN AND SET USER
     try {
       const { authenticatedPerson } = jwt.verify(token, secret, {
         maxAge: expiration,
@@ -30,11 +44,16 @@ module.exports = {
       console.log("Invalid token");
     }
 
+    // RETURN MODIFIED REQUEST
     return req;
   },
 
+  // SIGN TOKEN FUNCTION
   signToken: function ({ email, username, _id }) {
+    // CREATE PAYLOAD
     const payload = { email, username, _id };
+
+    // SIGN AND RETURN TOKEN
     return jwt.sign({ authenticatedPerson: payload }, secret, {
       expiresIn: expiration,
     });
