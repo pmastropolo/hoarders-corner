0e30e21344311d3b727623a9fd0a6c84505efcea
Peyton Mastropolo <amyhytwn@aol.com>
2023-11-28 00:24:16 -0600

    Update index.js
    comments

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,14 @@
+// IMPORT USER MODEL
 const User = require("./User");
+
+// IMPORT ITEM MODEL
 const Item = require("./Item");
+
+// IMPORT COMMUNITY MODEL
 const Community = require("./Community");
+
+// IMPORT MESSAGE MODEL
 const Message = require("./Message");
 
+// EXPORT MODELS
 module.exports = { User, Item, Community, Message };
