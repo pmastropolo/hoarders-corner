83ed1190d380a114a591ba549eea8a6aebfb7054
Peyton Mastropolo <amyhytwn@aol.com>
2023-11-28 19:09:54 -0600

    Update CommunityRow.jsx

--- client/src/components/CommunityRow/CommunityRow.jsx
+++ client/src/components/CommunityRow/CommunityRow.jsx
@@ -1,60 +1,61 @@
-import { useQuery, useMutation } from "@apollo/client"; // IMPORT APOLLO CLIENT HOOKS
-import Button from "../Atoms/Button";                   // IMPORT BUTTON COMPONENT
-import { LEAVE_COMMUNITY } from "../../utils/mutations"; // IMPORT LEAVE COMMUNITY MUTATION
+import { useQuery, useMutation } from "@apollo/client";
+import Button from "../Atoms/Button";
+import { LEAVE_COMMUNITY } from "../../utils/mutations";
 import {
   QUERY_MY_COMMUNITIES,
   QUERY_COMMUNITY_ITEMS,
-} from "../../utils/queries";                           // IMPORT GRAPHQL QUERIES
+} from "../../utils/queries";
 
-import { Link } from "react-router-dom"; // IMPORT LINK COMPONENT FROM REACT ROUTER
-import Auth from "../../utils/auth"; // IMPORT AUTHENTICATION UTILS
-import classes from "./communityrow.css"; // IMPORT CSS STYLES
+import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
+import classes from "./communityrow.css";
 
 export default function CommunityRow({
-  _id, // PROP: COMMUNITY ID
-  name, // PROP: COMMUNITY NAME
-  members, // PROP: COMMUNITY MEMBERS
-  items, // PROP: COMMUNITY ITEMS
-  tagline, // PROP: COMMUNITY TAGLINE
-  isMyCommunity, // PROP: FLAG IF IT'S USER'S COMMUNITY
-  join, // PROP: JOIN FUNCTION
-  hasButton, // PROP: FLAG TO SHOW/HIDE BUTTON
-  description, // PROP: COMMUNITY DESCRIPTION
+  _id,
+  name,
+  members,
+  items,
+  tagline,
+  isMyCommunity,
+  join,
+  hasButton,
+  description,
 }) {
 
-const [leaveCommunity, { err }] = useMutation(LEAVE_COMMUNITY, {
-  refetchQueries: [QUERY_MY_COMMUNITIES, "communities"], // INITIALIZE MUTATION WITH REFETCH QUERIES
-});
-const communityId = _id; // SET COMMUNITY ID
-const { loading, data, error } = useQuery(QUERY_COMMUNITY_ITEMS, { // FETCH COMMUNITY ITEMS
-  variables: { communityId: communityId }, // SET QUERY VARIABLES
-});
 
-const myCommunityItems = 
-  (Auth.loggedIn() && // CHECK IF USER IS LOGGED IN
-    data?.itemByCommunity.items.filter( // FILTER ITEMS OWNED BY USER
-      (item) => item.owner === Auth.getProfile().authenticatedPerson.username
-    )) ||
-  []; // DEFAULT TO EMPTY ARRAY IF CONDITION FAILS
+  const [leaveCommunity, { err }] = useMutation(LEAVE_COMMUNITY, {
+    refetchQueries: [QUERY_MY_COMMUNITIES, "communities"],
+  });
+  const communityId = _id;
+  const { loading, data, error } = useQuery(QUERY_COMMUNITY_ITEMS, {
+    variables: { communityId: communityId },
+  });
 
-const leaveCommunityAction = async (communityId, communityName) => { // ASYNC FUNCTION TO HANDLE LEAVING COMMUNITY
-  if (myCommunityItems.length > 0) { // CHECK IF USER HAS ITEMS IN COMMUNITY
-    alert("You have items in this community. Please remove them first"); // ALERT IF USER HAS ITEMS
-    return; // EXIT FUNCTION
-  } else {
-    try {
-      const { data } = await leaveCommunity({ variables: { communityId } }); // ATTEMPT TO LEAVE COMMUNITY
-    } catch (error) {
-      console.error(error); // CATCH AND LOG ERRORS
+  const myCommunityItems =
+    (Auth.loggedIn() &&
+      data?.itemByCommunity.items.filter(
+        (item) => item.owner === Auth.getProfile().authenticatedPerson.username
+      )) ||
+    [];
+
+  const leaveCommunityAction = async (communityId, communityName) => {
+    if (myCommunityItems.length > 0) {
+      alert("You have items in this community. Please remove them first");
+      return;
+    } else {
+      try {
+        const { data } = await leaveCommunity({ variables: { communityId } });
+      } catch (error) {
+        console.error(error);
+      }
+
+      if (communityId) {
+        alert(`You are no longer following ${communityName}`);
+      } else if (!communityId) {
+        alert("Didn't successfully leave");
+      }
     }
-
-    if (communityId) { // CHECK IF COMMUNITY ID EXISTS
-      alert(`You are no longer following ${communityName}`); // ALERT USER THEY LEFT COMMUNITY
-    } else if (!communityId) { // IF NO COMMUNITY ID
-      alert("Didn't successfully leave"); // ALERT FAILURE TO LEAVE
-    }
-  }
-};
+  };
 
   return (
   <div className="community-row">
@@ -68,7 +69,10 @@ const leaveCommunityAction = async (communityId, communityName) => { // ASYNC FU
           {description && <p className={`${classes.desc} desc`}>{description}</p>}
         </div>
       </div>
-     <div className="flex items-center min-w-[128px]">
+     
+
+
+      <div className="flex items-center min-w-[128px]">
         <i className="fa-solid fa-users mr-1 text-pri-5 "></i>
         <h4 className="text-h4 font-bold">{members} Members</h4>
       </div>
